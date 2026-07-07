62a5cff5007297a72ee253c80ccdfc08a765afaa
yasht-dev <ytailor9982@gmail.com>
2026-07-07 18:12:11 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss() ], 
+  plugins: [react(), tailwindcss() ,Sitemap({
+      hostname: 'https://thex-vision.vercel.app'
+    }) ], 
 })
